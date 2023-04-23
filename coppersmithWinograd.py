986c20bb77634c68aab55846e049a23c4032e16c
Xiaoyan He <88536944+XiaoyanHe0713@users.noreply.github.com>
2023-04-22 20:25:50 -0700

    Updated

--- coppersmithWinograd.py
+++ coppersmithWinograd.py
@@ -1,12 +1,12 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 # Implementation of the Coppersmith-Winograd algorithm for matrix multiplication
 class coppersmithWinograd:
 
-    def __init__(self, A, B, C, n):
-        self.A = A
-        self.B = B
-        self.C = C
+    def __init__(self, n, M):
         self.n = n
+        self.n = M
 
     def coppersmithWinograd(self, M1, M2, M3, n):
 
@@ -34,15 +34,31 @@ class coppersmithWinograd:
 
         return error
     
+    def run(self):
+        # Generate a random matrix
+        A = np.random.random((n, n))
+        I = np.identity(n)
+
+        error = []
+        for m in M:
+            A = np.dot(A, m)
+            B = np.linalg.inv(A)
+
+            error.append(self.coppersmithWinograd(A, B, I, n))
+
+        # plot the error with respect to M
+        plt.plot(M, error)
+        plt.xlabel("scalar")
+        plt.ylabel("error")
+        plt.show()
+
 if __name__ == "__main__":
     # test the algorithm
-    # random matrices
-    A = [[1,2],[3,4]]
-    B = [[2,0],[1,2]]
-    C = [[4,4],[10,8]]
-    n = 2
-    s = coppersmithWinograd(A, B, C, n)
+    n = 10
+    len = 40
 
-    print(s.coppersmithWinograd(A, B, C, n))
-
-        
+    # Generate a random array of scalars takes from the interval [10, 100]
+    M = np.random.randint(10, 200, size=(len))
+    M = np.sort(M)
+    s = coppersmithWinograd(n, M)
+    s.run()
